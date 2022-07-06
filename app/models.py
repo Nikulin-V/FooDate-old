f39d2ae0e48010fa7d7a1330018e3d662bcf3d77
Nikulin Vasiliy <nikulin.vasily.777@ya.ru>
2022-07-06 22:08:55 +0500

    Fixed validation

--- app/models.py
+++ app/models.py
@@ -1,14 +1,13 @@
-from datetime import datetime
+from uuid import uuid4
 
 from django.contrib.auth import get_user_model
 from django.core import validators
 from django.db import models
-# noinspection PyPackageRequirements
-from slugify import slugify
 
 from book.models import ProductCard
 from core.choices import AMOUNT_UNIT
 from core.middleware import get_user
+import core.validators
 
 User = get_user_model()
 
@@ -21,26 +20,23 @@ class Product(models.Model):
     products = ProductManager()
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=get_user)
-    slug = models.CharField(max_length=255, validators=[validators.validate_slug], unique=True,
-                            blank=True, null=True)
+    slug = models.CharField(max_length=255, validators=[validators.validate_slug],
+                            default=f'product-{uuid4()}')
     product_card = models.ForeignKey(
         ProductCard, on_delete=models.DO_NOTHING, related_name='products'
     )
-    amount = models.FloatField('Количество продукта', null=True, blank=True)
+    amount = models.FloatField('Количество продукта', null=True, blank=True, validators=[
+        core.validators.BiggerThanValidator(0)
+    ])
     amount_unit = models.CharField('Единица измерения количества продукта', choices=AMOUNT_UNIT,
                                    default=AMOUNT_UNIT[1], null=True, blank=True, max_length=2)
     production_date = models.DateTimeField('Дата изготовления', null=True, blank=True,
-                                           validators=[validators.MaxValueValidator(datetime.now)])
+                                           validators=[core.validators.date_in_past])
     purchase_date = models.DateTimeField('Дата покупки', null=True, blank=True,
-                                         validators=[validators.MaxValueValidator(datetime.now)])
-
-    def save(self, *args, **kwargs):
-        super(Product, self).save(*args, **kwargs)
-        self.slug = slugify(f'{self.product_card.name} {self.pk}')
-        super(Product, self).save(*args, **kwargs)
+                                         validators=[core.validators.date_in_past])
 
     def __str__(self):
-        return f'<Product> {self.pk} | {self.product_card.slug} {self.amount} {self.amount_unit}'
+        return self.slug
 
     class Meta:
         verbose_name = 'Продукт'
--- foodate/settings.py
+++ foodate/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Yekaterinburg'
 
 USE_I18N = True
 
