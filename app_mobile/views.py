c075c3d210a047040a1f6b0d110c600a54ea5e45
Nikulin Vasiliy <nikulin.vasily.777@ya.ru>
2022-07-19 16:07:17 +0500

    Added redirects

--- app_mobile/views.py
+++ app_mobile/views.py
@@ -1,24 +1,23 @@
 from django.shortcuts import render
 from django.views import View
+from django_hosts import reverse_lazy
+
+from core.decorators import pc_redirect
 
 
 class HomeView(View):
-    template = 'error_pages/development.html'
-    template_mobile = 'error_pages/development.html'
+    template = 'app_mobile/homepage.html'
+    pc_reverse = reverse_lazy('home', host='app')
 
+    @pc_redirect(pc_reverse)
     def get(self, request):
-        return render(
-            request,
-            self.template if request.user_agent.is_pc else self.template_mobile
-        )
+        return render(request, self.template)
 
 
 class AppView(View):
     template = 'error_pages/development.html'
-    template_mobile = 'error_pages/development.html'
+    pc_reverse = reverse_lazy('app', host='app')
 
+    @pc_redirect(pc_reverse)
     def get(self, request):
-        return render(
-            request,
-            self.template if request.user_agent.is_pc else self.template_mobile
-        )
+        return render(request, self.template)
