36a3da7d55dc6d573ae492e409de9529a20844a0
Nikulin Vasiliy <nikulin.vasily.777@ya.ru>
2022-07-20 15:13:04 +0500

    Added app homepage (#41)
    * Added app homepage
    
    * Text update
    
    * Added redirects
    
    * Added mobile homepage
    
    * Edited headers
    
    * Edited header

--- app/views.py
+++ app/views.py
@@ -1,38 +1,36 @@
 from django.shortcuts import render
 from django.views import View
+from django_hosts import reverse_lazy
 
 from app.forms import NewProductForm
 from app.models import Product
+from core.decorators import mobile_redirect
 
 
 class HomeView(View):
-    template = 'error_pages/development.html'
-    template_mobile = 'error_pages/development.html'
+    template = 'app/homepage.html'
+    mobile_reverse = reverse_lazy('home', host='app_mobile')
 
+    @mobile_redirect(mobile_reverse)
     def get(self, request):
-        return render(
-            request,
-            self.template if request.user_agent.is_pc else self.template_mobile
-        )
+        return render(request, self.template)
 
 
 class AppView(View):
     template = 'app/app.html'
-    template_mobile = 'error_pages/development.html'
     form = NewProductForm
+    mobile_reverse = reverse_lazy('app', host='app_mobile')
 
+    @mobile_redirect(mobile_reverse)
     def get(self, request, saved=None):
         context = {
             'form': self.form() if saved is None else self.form(request.POST),
             'saved': saved
         }
 
-        return render(
-            request,
-            self.template if request.user_agent.is_pc else self.template_mobile,
-            context
-        )
+        return render(request, self.template, context)
 
+    @mobile_redirect(mobile_reverse)
     def post(self, request):
         form = self.form(request.POST)
         saved = False
---
+++ core/decorators.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponseRedirect
+
+
+def pc_redirect(redirect_url):
+    def pc_redirect_decorator(func):
+        def _wrapper(*args, **kwargs):
+            request = args[1]
+            if request.user_agent.is_pc:
+                return HttpResponseRedirect(redirect_url)
+            return func(*args, **kwargs)
+        return _wrapper
+    return pc_redirect_decorator
+
+
+def mobile_redirect(redirect_url):
+    def mobile_redirect_decorator(func):
+        def _wrapper(*args, **kwargs):
+            request = args[1]
+            if request.user_agent.is_mobile or request.user_agent.is_tablet:
+                return HttpResponseRedirect(redirect_url)
+            return func(*args, **kwargs)
+        return _wrapper
+    return mobile_redirect_decorator
