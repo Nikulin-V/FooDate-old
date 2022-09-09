3a076c0596367f95d2f3764a9d8ceb1562350c35
Vasiliy Nikulin <nikulin.vasily.777@ya.ru>
2022-09-10 01:09:41 +0300

    Added sitemap.xml and robots.txt

---
+++ app/sitemaps.py
@@ -0,0 +1,35 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+
+class HomeViewSitemap(Sitemap):
+    changefreq = 'monthly'
+    priority = 1.0
+
+    def items(self):
+        return ['home']
+
+    def location(self, item):
+        return reverse(item)
+
+
+class AuthViewSitemap(Sitemap):
+    changefreq = 'monthly'
+    priority = 0.9
+
+    def items(self):
+        return ['login', 'register']
+
+    def location(self, item):
+        return reverse(item)
+
+
+class AppViewSitemap(Sitemap):
+    changefreq = 'daily'
+    priority = 0.5
+
+    def items(self):
+        return ['app']
+
+    def location(self, item):
+        return reverse(item)
