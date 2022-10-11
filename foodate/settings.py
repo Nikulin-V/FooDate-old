30de557dc5caa368c6a80696f82e6dba0cffb03a
Vasiliy Nikulin <nikulin.vasily.777@ya.ru>
2022-10-11 19:08:50 +0300

    Update social-auth-app config

--- foodate/settings.py
+++ foodate/settings.py
@@ -157,7 +157,17 @@ AUTHENTICATION_BACKENDS = [
 
 # Social auth
 
-SOCIAL_AUTH_PROVIDERS = ['vk-oauth2']
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
 
 SOCIAL_AUTH_VK_OAUTH2_KEY = os.getenv('VK_APP_ID')
 SOCIAL_AUTH_VK_OAUTH2_SECRET = os.getenv('VK_API_SECRET')
