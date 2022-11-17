b0e247b744eba43dd0f5449482e2b70364598155
Vasiliy Nikulin <nikulin.vasily.777@ya.ru>
2022-11-18 00:32:43 +0300

    Added ability to influence product development

--- users/models.py
+++ users/models.py
@@ -5,6 +5,11 @@ from django.db import models
 class User(AbstractUser):
     """Custom user model"""
     is_email_verified = models.BooleanField('Почта подтверждена', default=False)
+    beta_testing = models.BooleanField('Хочу участвовать в бета-тестировании', default=False)
+    customer_development_interview = models.BooleanField(
+        'Готов поговорить об опыте использования похожих сервисов (доставка, кулинария, фудшеринг)',
+        default=False
+    )
 
     @property
     def is_guest(self):
